7e45d9dd1047ee9a7d8b07d18ecf119e2a325206
Adauto Júnior <adautodcjunior@gmail.com>
2022-12-04 17:37:56 -0300

    finishing class 03

---
+++ listener_twitter.py
@@ -0,0 +1,32 @@
+import os
+from socket import socket
+import tweepy
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+host = os.getenv('HOST')
+port = int(os.getenv('PORT'))
+token = os.getenv('TOKEN')
+
+s = socket()
+s.bind((host, port))
+print(f'Aguardando conexão na porta: {port}')
+
+s.listen(5)
+connection, address = s.accept()
+print(f'Recebendo solicitação de {address}')
+
+keyword = 'copa'
+
+class GetTweets(tweepy.StreamingClient):
+  def on_tweet(self, tweet):
+    print(tweet.text)
+    print('='*50)
+    connection.send(tweet.text.encode('latin1', 'ignore'))
+
+printer = GetTweets(token)
+printer.add_rules(tweepy.StreamRule(keyword))
+printer.filter()
+
+connection.close()
